814eaf578947503391c6077f152dcc77ace9a096
Duane Ellis <duane@duaneellis.com>
2018-11-01 22:20:32 -0700

    Add initial script

---
+++ 000_step1.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+sudo apt-get update
+echo "dash dash/sh boolean false" | sudo debconf-set-selections
+
+sudo apt-get install -y git
+sudo apt-get install -y emacs 
+sudo apt-get install -y build-essential 
+sudo apt-get install -y openssh-server
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-setuptools 
+sudo apt-get install -y python-dev 
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+
+
+sudo apt-get install -y vim-gtk3-py2
